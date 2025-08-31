4816cdb913bb5db0ed5ffc950afaae3aa60d7f71
David Thole <david@thedarktrumpet.com>
2025-08-31 06:52:54 -0500

    Initital commit - adding plugin to verify the most common (at least for me) options exist for venv or .venv

---
+++ venv-autoactivate.plugin.zsh
@@ -0,0 +1,22 @@
+function _activate_env_if_exists() {
+  if [[ -z "$VIRTUAL_ENV" ]] ; then
+    ## If env folder is found then activate the vitualenv
+      if [[ -d ./.venv ]] ; then
+          source ./.venv/bin/activate
+      elif [[ -d ./venv ]] ; then
+	  source ./venv/bin/activate
+      fi
+  else
+    ## check the current folder belong to earlier VIRTUAL_ENV folder
+    # if yes then do nothing
+    # else deactivate
+      parentdir="$(dirname "$VIRTUAL_ENV")"
+      if [[ "$PWD"/ != "$parentdir"/* ]] ; then
+        deactivate
+      fi
+  fi
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook chpwd _activate_env_if_exists
+_activate_env_if_exists # Initial call to check the current directory at shell startup
